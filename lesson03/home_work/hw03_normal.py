a54f14328e75c15cdd460cfe013bc92457fae2be
Cerzon <kirill.tkachenko@gmail.com>
2019-09-24 18:24:50 +0300

    lesson03 hw_normal -1

--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -6,7 +6,22 @@ __author__ = 'Ткаченко Кирилл Павлович'
 # Первыми элементами ряда считать цифры 1 1
 
 def fibonacci(n, m):
-    pass
+    """
+    Подсчёт элементов ряда Фибоначчи с возвращением элементов
+    начиная с n и заканчивая m
+    """
+    prev, curr = (1, 1,)
+    result = []
+    if n == 1 and m >= 2:
+        result = [1, 1,]
+    elif n == m == 1 or n == 2:
+        result = [1,]
+    for i in range(3, m + 1):
+        prev, curr = (curr, prev + curr)
+        if i >= n:
+            result.append(curr)
+    return result
+
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
@@ -15,13 +30,36 @@ def fibonacci(n, m):
 
 
 def sort_to_max(origin_list):
-    pass
+    """
+    Принимаем список и возвращаем список, отсортированный по возрастанию
+    """
+    result = []
+    for item in origin_list:
+        for i in range(len(result)):
+            if item <= result[i]:
+                result.insert(i, item)
+                break
+            if item >= result[len(result) - i - 1]:
+                result.insert(len(result) - i, item)
+                break
+        else:
+            result.append(item)
+    return result
+
 
 sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
+
+
+def my_filter(iter_to_test):
+    """
+    Возвращаем список (можно бы и просто генератор) с ненулевыми
+    и непустыми элементами изначального
+    """
+    return [x for x in iter_to_test if x]
 
 
 # Задача-4:
