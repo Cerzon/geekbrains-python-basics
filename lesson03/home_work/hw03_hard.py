52fcfab310a32a39b9c6427d6d23f781c127b351
Cerzon <kirill.tkachenko@gmail.com>
2019-09-25 11:18:58 +0300

    lesson03 hw_hard started

--- lesson03/home_work/hw03_hard.py
+++ lesson03/home_work/hw03_hard.py
@@ -12,6 +12,78 @@ __author__ = 'Ткаченко Кирилл Павлович'
 # Ввод: -2/3 - -2
 # Вывод: 1 1/3
 
+import re
+
+class MyFraction:
+    def __init__(self, str_fraction):
+        match = re.match(
+            r'^(?P<entier>-?\d+\b)? ?((?P<dividend>(?(entier)|-?)\d+)/(?P<divider>\d+))?$',
+            str_fraction)
+        if match is None:
+            return None
+        self.negative = False
+        self.entier = 0
+        self.dividend = 0
+        self.divider = 1
+        if match.group('entier'):
+            self.entier = int(match.group('entier'))
+            if self.entier < 0:
+                self.negative = True
+                self.entier *= -1
+        if match.group('dividend'):
+            self.dividend = int(match.group('dividend'))
+            if self.dividend < 0:
+                self.negative = True
+                self.dividend *= -1
+            self.divider = int(match.group('divider'))
+            if self.divider == 0:
+                raise ZeroDivisionError
+        self.simplify()
+
+    def simplify(self):
+        self.entier += self.dividend // self.divider
+        self.dividend = self.dividend % self.divider
+        for i in range(1, self.dividend // 2):
+            if not self.dividend % i and not self.divider % (self.dividend // i):
+                self.divider //= self.dividend // i
+                self.dividend = i
+                break
+        if self.dividend == 0:
+            self.divider = 1
+
+    def __str__(self):
+        str_output = ''
+        if self.entier:
+            str_output = str(self.entier * (-1 if self.negative else 1))
+        if self.dividend:
+            if len(str_output):
+                str_output += ' '
+            else:
+                str_output = '-' if self.negative else ''
+            str_output += '{0}/{1}'.format(self.dividend, self.divider)
+        return str_output
+
+    def __add__(self, other):
+        pass
+
+    def __sub__(self, other):
+        pass
+
+    def __mul__(self, other):
+        pass
+
+    def __truediv__(self, other):
+        pass
+
+
+var1 = MyFraction('-54 125/36')
+print(var1)
+print('-' * 70)
+var1 = MyFraction('3 5/55')
+print(var1)
+print('-' * 70)
+var1 = MyFraction('-144/24')
+print(var1, var1.dividend, var1.divider)
 
 # Задание-2:
 # Дана ведомость расчета заработной платы (файл "data/workers").
