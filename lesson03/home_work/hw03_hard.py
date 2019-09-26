842e94aefdb6301f313655fe545ac56a0d74b7ae
Cerzon <kirill.tkachenko@gmail.com>
2019-09-26 04:01:31 +0300

    lesson03 hw_hard +

--- lesson03/home_work/hw03_hard.py
+++ lesson03/home_work/hw03_hard.py
@@ -23,10 +23,10 @@ class MyFraction:
     supports add, sub, mul, truediv, comparison
     """
     def __init__(self, *args):
-        self.negative = False
-        self.entier = 0
-        self.dividend = 0
-        self.divider = 1
+        self._negative = False
+        self._entier = 0
+        self._dividend = 0
+        self._divider = 1
         if isinstance(args[0], str):
             match = re.match(
                 r'^(?P<entier>-?\d+\b)? ?((?P<dividend>(?(entier)|-?)\d+)/(?P<divider>\d+))?$',
@@ -34,80 +34,78 @@ class MyFraction:
             if match is None:
                 raise ValueError('Wrong fraction format. "{}" does not match to "[-][<int:entier>] [<int:dividend>/<int:divider>]"'.format(args[0]))
             if match.group('entier'):
-                self.entier = int(match.group('entier'))
+                self._entier = int(match.group('entier'))
             if match.group('dividend'):
-                self.dividend = int(match.group('dividend'))
-                self.divider = int(match.group('divider'))
+                self._dividend = int(match.group('dividend'))
+                self._divider = int(match.group('divider'))
         elif len(args) == 3 and not False in (isinstance(x, int) for x in args):
-            self.entier, self.dividend, self.divider = args
-            if self.divider < 0:
+            self._entier, self._dividend, self._divider = args
+            if self._divider < 0:
                 raise ValueError('Wrong fraction format. Divider must be positive')
         else:
             return None
-        if self.divider == 0:
+        if self._divider == 0:
             raise ValueError('Divider can not be zero')
-        if self.entier < 0:
-            self.negative = True
-            self.entier *= -1
-        if self.dividend < 0:
-            self.negative = True
-            self.dividend *= -1
+        if self._entier < 0:
+            self._negative = True
+            self._entier *= -1
+        if self._dividend < 0:
+            self._negative = True
+            self._dividend *= -1
         self.simplify()
 
     def simplify(self):
-        self.entier += self.dividend // self.divider
-        self.dividend = self.dividend % self.divider
-        for i in range(1, self.dividend // 2 + 1):
-            if not self.dividend % i and not self.divider % (self.dividend // i):
-                self.divider //= self.dividend // i
-                self.dividend = i
+        self._entier += self._dividend // self._divider
+        self._dividend = self._dividend % self._divider
+        for i in range(1, self._dividend // 2 + 1):
+            if not self._dividend % i and not self._divider % (self._dividend // i):
+                self._divider //= self._dividend // i
+                self._dividend = i
                 break
-        if self.dividend == 0:
-            self.divider = 1
+        if self._dividend == 0:
+            self._divider = 1
 
     def complicate(self):
-        self.dividend += self.entier * self.divider
-        self.entier = 0
+        self._dividend += self._entier * self._divider
+        self._entier = 0
 
     def __str__(self):
         str_output = ''
-        if self.entier:
-            str_output = str(self.entier * (-1 if self.negative else 1))
-        if self.dividend:
+        if self._entier:
+            str_output = str(self._entier * (-1 if self._negative else 1))
+        if self._dividend:
             if len(str_output):
                 str_output += ' '
             else:
-                str_output = '-' if self.negative else ''
-            str_output += '{0}/{1}'.format(self.dividend, self.divider)
+                str_output = '-' if self._negative else ''
+            str_output += '{0}/{1}'.format(self._dividend, self._divider)
         return str_output
 
     def __add__(self, other):
         if not isinstance(other, MyFraction):
             if isinstance(other, int):
-                return MyFraction(self.entier * (-1 if self.negative else 1) + other,
-                    self.dividend, self.divider)
+                other = MyFraction(other, 0, 1)
             else:
                 return NotImplemented
         self.complicate()
         other.complicate()
-        res_dividend = self.dividend * (-1 if self.negative else 1) * other.divider + other.dividend * (-1 if other.negative else 1) * self.divider
+        res_dividend = self._dividend * (-1 if self._negative else 1) * other._divider + other._dividend * (-1 if other._negative else 1) * self._divider
         self.simplify()
         other.simplify()
-        return MyFraction(0, res_dividend, self.divider * other.divider)
+        return MyFraction(0, res_dividend, self._divider * other._divider)
 
     def __sub__(self, other):
         if not isinstance(other, MyFraction):
             if isinstance(other, int):
-                return MyFraction(self.entier * (-1 if self.negative else 1) - other,
-                    self.dividend, self.divider)
+                other = MyFraction(other, 0, 1)
             else:
                 return NotImplemented
         self.complicate()
         other.complicate()
-        res_dividend = self.dividend * (-1 if self.negative else 1) * other.divider - other.dividend * (-1 if other.negative else 1) * self.divider
+        res_dividend = self._dividend * (-1 if self._negative else 1) * other._divider - other._dividend * (-1 if other._negative else 1) * self._divider
         self.simplify()
         other.simplify()
-        return MyFraction(0, res_dividend, self.divider * other.divider)
+        return MyFraction(0, res_dividend, self._divider * other._divider)
 
     def __mul__(self, other):
         if not isinstance(other, MyFraction):
@@ -117,10 +115,10 @@ class MyFraction:
                 return NotImplemented
         self.complicate()
         other.complicate()
-        res_dividend = self.dividend * (-1 if self.negative else 1) * other.dividend * (-1 if other.negative else 1)
+        res_dividend = self._dividend * (-1 if self._negative else 1) * other._dividend * (-1 if other._negative else 1)
         self.simplify()
         other.simplify()
-        return MyFraction(0, res_dividend, self.divider * other.divider)
+        return MyFraction(0, res_dividend, self._divider * other._divider)
 
     def __truediv__(self, other):
         if not isinstance(other, MyFraction):
@@ -130,8 +128,8 @@ class MyFraction:
                 return NotImplemented
         self.complicate()
         other.complicate()
-        res_dividend = self.dividend * (-1 if self.negative else 1) * other.divider * (-1 if other.negative else 1)
-        res_divider = self.divider * other.dividend
+        res_dividend = self._dividend * (-1 if self._negative else 1) * other._divider * (-1 if other._negative else 1)
+        res_divider = self._divider * other._dividend
         self.simplify()
         other.simplify()
         return MyFraction(0, res_dividend, res_divider)
@@ -144,7 +142,7 @@ class MyFraction:
                 return NotImplemented
         self.complicate()
         other.complicate()
-        result = self.dividend * (-1 if self.negative else 1) * other.divider < other.dividend * (-1 if other.negative else 1) * self.divider
+        result = self._dividend * (-1 if self._negative else 1) * other._divider < other._dividend * (-1 if other._negative else 1) * self._divider
         self.simplify()
         other.simplify()
         return result
@@ -157,7 +155,7 @@ class MyFraction:
                 return NotImplemented
         self.complicate()
         other.complicate()
-        result = self.dividend * (-1 if self.negative else 1) * other.divider <= other.dividend * (-1 if other.negative else 1) * self.divider
+        result = self._dividend * (-1 if self._negative else 1) * other._divider <= other._dividend * (-1 if other._negative else 1) * self._divider
         self.simplify()
         other.simplify()
         return result
@@ -170,7 +168,7 @@ class MyFraction:
                 return NotImplemented
         self.complicate()
         other.complicate()
-        result = self.dividend * (-1 if self.negative else 1) * other.divider > other.dividend * (-1 if other.negative else 1) * self.divider
+        result = self._dividend * (-1 if self._negative else 1) * other._divider > other._dividend * (-1 if other._negative else 1) * self._divider
         self.simplify()
         other.simplify()
         return result
@@ -183,7 +181,7 @@ class MyFraction:
                 return NotImplemented
         self.complicate()
         other.complicate()
-        result = self.dividend * (-1 if self.negative else 1) * other.divider >= other.dividend * (-1 if other.negative else 1) * self.divider
+        result = self._dividend * (-1 if self._negative else 1) * other._divider >= other._dividend * (-1 if other._negative else 1) * self._divider
         self.simplify()
         other.simplify()
         return result
@@ -196,7 +194,7 @@ class MyFraction:
                 return NotImplemented
         self.complicate()
         other.complicate()
-        result = self.dividend * (-1 if self.negative else 1) * other.divider == other.dividend * (-1 if other.negative else 1) * self.divider
+        result = self._dividend * (-1 if self._negative else 1) * other._divider == other._dividend * (-1 if other._negative else 1) * self._divider
         self.simplify()
         other.simplify()
         return result
@@ -209,47 +207,46 @@ class MyFraction:
                 return NotImplemented
         self.complicate()
         other.complicate()
-        result = self.dividend * (-1 if self.negative else 1) * other.divider != other.dividend * (-1 if other.negative else 1) * self.divider
+        result = self._dividend * (-1 if self._negative else 1) * other._divider != other._dividend * (-1 if other._negative else 1) * self._divider
         self.simplify()
         other.simplify()
         return result
 
 
-if __name__ == '__main__':
-    print('*' * 70)
-    print('Работаем с простыми дробями')
-    str_input = input('Введите операцию с двумя дробными числами: ').strip()
-    if str_input.find(' + ') > 0:
-        first, second = (x.strip() for x in str_input.split(' + '))
-        print(str_input, '=', MyFraction(first) + MyFraction(second))
-    if str_input.find(' - ') > 0:
-        first, second = (x.strip() for x in str_input.split(' - '))
-        print(str_input, '=', MyFraction(first) - MyFraction(second))
-    if str_input.find(' * ') > 0:
-        first, second = (x.strip() for x in str_input.split(' * '))
-        print(str_input, '=', MyFraction(first) * MyFraction(second))
-    if str_input.find(' / ') > 0:
-        first, second = (x.strip() for x in str_input.split(' / '))
-        print(str_input, '=', MyFraction(first) / MyFraction(second))
-    if str_input.find(' > ') > 0:
-        first, second = (x.strip() for x in str_input.split(' > '))
-        print(str_input, '=', MyFraction(first) > MyFraction(second))
-    if str_input.find(' >= ') > 0:
-        first, second = (x.strip() for x in str_input.split(' >= '))
-        print(str_input, '=', MyFraction(first) >= MyFraction(second))
-    if str_input.find(' < ') > 0:
-        first, second = (x.strip() for x in str_input.split(' < '))
-        print(str_input, '=', MyFraction(first) < MyFraction(second))
-    if str_input.find(' <= ') > 0:
-        first, second = (x.strip() for x in str_input.split(' <= '))
-        print(str_input, '=', MyFraction(first) <= MyFraction(second))
-    if str_input.find(' == ') > 0:
-        first, second = (x.strip() for x in str_input.split(' == '))
-        print(str_input, '=', MyFraction(first) == MyFraction(second))
-    if str_input.find(' != ') > 0:
-        first, second = (x.strip() for x in str_input.split(' != '))
-        print(str_input, '=', MyFraction(first) != MyFraction(second))
-    print('*' * 70)
+print('*' * 70)
+print('Работаем с простыми дробями')
+str_input = input('Введите операцию с двумя дробными числами: ').strip()
+if str_input.find(' + ') > 0:
+    first, second = (x.strip() for x in str_input.split(' + '))
+    print(str_input, '=', MyFraction(first) + MyFraction(second))
+if str_input.find(' - ') > 0:
+    first, second = (x.strip() for x in str_input.split(' - '))
+    print(str_input, '=', MyFraction(first) - MyFraction(second))
+if str_input.find(' * ') > 0:
+    first, second = (x.strip() for x in str_input.split(' * '))
+    print(str_input, '=', MyFraction(first) * MyFraction(second))
+if str_input.find(' / ') > 0:
+    first, second = (x.strip() for x in str_input.split(' / '))
+    print(str_input, '=', MyFraction(first) / MyFraction(second))
+if str_input.find(' > ') > 0:
+    first, second = (x.strip() for x in str_input.split(' > '))
+    print(str_input, '=', MyFraction(first) > MyFraction(second))
+if str_input.find(' >= ') > 0:
+    first, second = (x.strip() for x in str_input.split(' >= '))
+    print(str_input, '=', MyFraction(first) >= MyFraction(second))
+if str_input.find(' < ') > 0:
+    first, second = (x.strip() for x in str_input.split(' < '))
+    print(str_input, '=', MyFraction(first) < MyFraction(second))
+if str_input.find(' <= ') > 0:
+    first, second = (x.strip() for x in str_input.split(' <= '))
+    print(str_input, '=', MyFraction(first) <= MyFraction(second))
+if str_input.find(' == ') > 0:
+    first, second = (x.strip() for x in str_input.split(' == '))
+    print(str_input, '=', MyFraction(first) == MyFraction(second))
+if str_input.find(' != ') > 0:
+    first, second = (x.strip() for x in str_input.split(' != '))
+    print(str_input, '=', MyFraction(first) != MyFraction(second))
+print('*' * 70)
 
 
 # Задание-2:
@@ -259,6 +256,65 @@ if __name__ == '__main__':
 # то их ЗП уменьшается пропорционально, а за заждый час переработки
 # они получают удвоенную ЗП, пропорциональную норме.
 # Кол-во часов, которые были отработаны, указаны в файле "data/hours_of"
+
+
+import os
+
+
+if __name__ == '__main__':
+    print('*' * 70)
+    print('Считаем отработанное время и чужие деньги')
+    answer = input('Выполнить чтение списка и запись по отдельным файлам? [Y/N] ').lower()
+    if answer == 'y' or answer == 'д':
+        employee_list = []
+        pattern = re.compile(r'^([а-яА-Я]+\b)[ \t]+(\b[-а-яА-Я]+\b)[ \t]+(\b\d+\b)[ \t]+(\b[-а-яА-Я]+\b)[ \t]+(\b\d+)$')
+        with open(os.path.join('data', 'workers'), 'r', encoding='utf-8') as employees:
+            for record in (line.strip('\n ') for line in employees if len(line.strip('\n '))):
+                match = pattern.match(record)
+                if match:
+                    employee_list.append(dict(zip(
+                        ('first_name', 'last_name', 'salary', 'position', 'hours',),
+                        (int(x) if x.isnumeric() else x for x in match.groups(default=None)))))
+
+        with open(os.path.join('data', 'hours_of'), 'r', encoding='utf-8') as worked_hours:
+            for record in (line.strip('\n ') for line in worked_hours if len(line.strip('\n '))):
+                for employee in employee_list:
+                    if (employee['first_name'] in record and
+                        employee['last_name'] in record):
+                        match = re.match(
+                            r'^{0}[ \t]+{1}[ \t]+(\b\d+)$'.format(employee['first_name'],
+                                employee['last_name']), record)
+                        employee['worked_out'] = int(match.group(1))
+                        employee['pay_out'] = int(employee['salary'] * (
+                            1 + (employee['worked_out'] / employee['hours'] - 1) * (
+                                2 if employee['worked_out'] > employee['hours'] else 1)))
+
+        # fn_wdth = max(len(emval) for emp in employee_list for emkey, emval in emp.items() if emkey == 'first_name')
+        # ln_wdth = max(len(emval) for emp in employee_list for emkey, emval in emp.items() if emkey == 'last_name')
+        # ps_wdth = max(len(emval) for emp in employee_list for emkey, emval in emp.items() if emkey == 'position')
+        # sl_wdth = max(len(str(emval)) for emp in employee_list for emkey, emval in emp.items() if emkey == 'salary')
+        # hr_wdth = max(len(str(emval)) for emp in employee_list for emkey, emval in emp.items() if emkey == 'hours')
+        # wo_wdth = max(len(str(emval)) for emp in employee_list for emkey, emval in emp.items() if emkey == 'worked_out')
+        # po_wdth = max(len(str(emval)) for emp in employee_list for emkey, emval in emp.items() if emkey == 'pay_out')
+        # for employee in employee_list:
+        #     print(
+        #         '{0:<{fn_wdth}} {1:<{ln_wdth}}  {2:<{ps_wdth}}  {3:>{sl_wdth}}  {4:>{hr_wdth}}  {5:>{wo_wdth}}  {6:>{po_wdth}}'.format(
+        #             employee['first_name'],
+        #             employee['last_name'],
+        #             employee['position'],
+        #             employee['salary'],
+        #             employee['hours'],
+        #             employee['worked_out'],
+        #             employee['pay_out'],
+        #             fn_wdth=fn_wdth,
+        #             ln_wdth=ln_wdth,
+        #             ps_wdth=ps_wdth,
+        #             sl_wdth=sl_wdth,
+        #             hr_wdth=hr_wdth,
+        #             wo_wdth=wo_wdth,
+        #             po_wdth=po_wdth
+        #         )
+        #     )
 
 
 # Задание-3:
@@ -273,3 +329,15 @@ if __name__ == '__main__':
 # Подсказка:
 # Чтобы получить список больших букв русского алфавита:
 # print(list(map(chr, range(ord('А'), ord('Я')+1))))
+
+
+if __name__ == '__main__':
+    print('*' * 70)
+    print('Читаем и пишем текстовые файлы')
+    answer = input('Выполнить чтение списка и запись по отдельным файлам? [Y/N] ').lower()
+    if answer == 'y' or answer == 'д':
+        with open(os.path.join('data', 'fruits.txt'), 'r', encoding='utf-8') as datafile:
+            for fruit in (line.strip('\n') for line in datafile if len(line.strip('\n'))):
+                with open(os.path.join('data', 'fruits_' + fruit[0].upper() + '.txt'), 'a', encoding='utf-8') as fruitfile:
+                    fruitfile.write(fruit + '\n')
+    print('*' * 70)
