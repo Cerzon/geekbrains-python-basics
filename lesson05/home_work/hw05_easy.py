e57efd40ec8b1dd026efbc73270df13c986f289c
Cerzon <kirill.tkachenko@gmail.com>
2019-10-07 02:36:09 +0300

    lesson05 hw_easy -1

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -9,44 +9,69 @@ __author__ = 'Ткаченко Кирилл Павлович'
 import os
 
 
-def folder_exist(fld_name):
-    """
-    Проверяет, существует ли папка с заданным именем.
-    """
-    pass
-
-
 def make_folder(fld_name):
     """
     Создаёт в текущей папку с заданным именем, если такой папки ещё нет.
     """
-    pass
+    if os.path.exists(fld_name) and os.path.isdir(fld_name):
+        print('Folder {} already exists'.format(fld_name))
+        return False
+    elif not os.path.isdir(os.path.dirname(fld_name)):
+        print('Parent folder {} does not exist'.format(os.path.dirname(fld_name)))
+        return False
+    os.mkdir(fld_name)
+    print('Folder {} created'.format(fld_name))
+    return True
 
 
 def remove_folder(fld_name):
     """
     Удаляет из текущей папку с заданным именем, если такая папка есть.
     """
-    pass
+    if os.path.isdir(fld_name):
+        if os.listdir(fld_name):
+            print('Folder {} is not empty'.format(fld_name))
+            return False
+        os.rmdir(fld_name)
+        print('Folder {} deleted'.format(fld_name))
+        return True
+    print('Folder {} does not exist'.format(fld_name))
+    return False
 
 
-for dir_name in ('dir_' + str(x) for x in range(1, 10)):
-    if make_folder(dir_name):
-        pass
-    else:
-        pass
+# создаём папки dir_1 - dir_9 в папке, из которой запущен скрипт
+# name_tpl = os.path.join(os.getcwd(), 'dir_')
+# for folder_name in (name_tpl + str(x) for x in range(1, 10)):
+#     make_folder(folder_name)
+
+# print('*' * 80)
+
+# # удаляем папки dir_1 - dir_9 в папке, из которой запущен скрипт
+# for folder_name in (name_tpl + str(x) for x in range(1, 10)):
+#     remove_folder(folder_name)
 
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
-def list_folder(fld_name=os.getcwd(), folders_only=True):
+def list_folder(fld_name=os.curdir, folders_only=False):
     """
     Выводит содержимое папки с заданным именем.
     folders_only=True -- вывести только имена папок.
     """
-    pass
+    if not os.path.isabs(fld_name):
+        fld_name = os.path.join(os.path.abspath(os.curdir), fld_name)
+    if os.path.isdir(fld_name) and os.listdir(fld_name):
+        for name in os.listdir(fld_name):
+            if os.path.isdir(os.path.join(fld_name, name)):
+                print('[{}]'.format(name))
+            elif not folders_only:
+                print(name)
 
+
+print('*' * 80)
+
+list_folder(folders_only=True)
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
