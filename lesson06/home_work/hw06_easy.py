2e414cec929f6a09d7163a7248e30ab78cdace40
Cerzon <kirill.tkachenko@gmail.com>
2019-10-11 18:00:10 +0300

    lesson06 hw_easy 1/2

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -16,28 +16,60 @@ def isflatpoint(var):
 
 
 class Triangle:
+    """
+    Треугольник задаётся координатами трёх его вершин -
+    a, b и c, представленными в видё кортежей (x, y)
+    """
 
-    def __init__(self, p1, p2, p3):
-        if not all(map(isflatpoint, (p1, p2, p3,))):
+    def __init__(self, a, b, c):
+        if not all(map(isflatpoint, (a, b, c,))):
             raise TypeError('Точка должна быть списком или кортежем координат x и y')
-        self._p_a, self._p_b, self._p_c = sorted([p1, p2, p3])
+        if any((a == b, a == c, b == c)):
+            raise ValueError('Минимум две из заданных точек совпадают')
+        self._a, self._b, self._c = a, b, c
+        if any(self.height(base) == 0 for base in ('ab', 'ac', 'bc')):
+            raise ValueError('Заданные точки образуют треугольник, вырожденный в линию')
 
+    @property
     def _sides(self):
-        len_ab = math.sqrt((self._p_b[0] - self._p_a[0])**2 + (self._p_b[1] - self._p_a[1])**2)
-        len_ac = math.sqrt((self._p_c[0] - self._p_a[0])**2 + (self._p_c[1] - self._p_a[1])**2)
-        len_bc = math.sqrt((self._p_c[0] - self._p_b[0])**2 + (self._p_c[1] - self._p_b[1])**2)
+        len_ab = math.sqrt((self._b[0] - self._a[0])**2 + (self._b[1] - self._a[1])**2)
+        len_ac = math.sqrt((self._c[0] - self._a[0])**2 + (self._c[1] - self._a[1])**2)
+        len_bc = math.sqrt((self._c[0] - self._b[0])**2 + (self._c[1] - self._b[1])**2)
         return len_ab, len_ac, len_bc
-
-    def height(self):
-        pass
-        # len_ab, len_ac, len_bc = self._sides()
-        # angle_ac = math.asin((self._p_c[1] - self._p_a[1]) / len_ac)
-        # angle_bc = math.asin((self._p_c[1] - self._p_b[1]) / len_bc)
-        # angle_acb = abs(angle_bc - angle_ac)
 
     @property
     def perimeter(self):
-        return sum(self._sides())
+        return sum(self._sides)
+
+    @property
+    def square(self):
+        hp = self.perimeter / 2
+        len_ab, len_ac, len_bc = self._sides
+        return math.sqrt(hp * (hp - len_ab) * (hp - len_bc) * (hp - len_ac))
+
+    def height(self, base):
+        """
+        строковый параметр, обозначающий сторону треугольника,
+        к которой проведена высота - ab, ac или bc
+        """
+        bases = dict(list(zip(['ab', 'ac', 'bc'], self._sides)))
+        if not base in bases:
+            raise ValueError('Неверно задан параметр функции height - {}'.format(base))
+        return self.square * 2 / bases[base]
+
+    def __str__(self):
+        return 'Треугольник a {} - b {} - c {}'.format(self._a, self._b, self._c)
+
+
+if __name__ == '__main__':
+    triag1 = Triangle((0, 10), (0, 0), (10, 0))
+    print(triag1)
+    print('Периметр =', triag1.perimeter)
+    print('Площадь =', triag1.square)
+    print('Высота из вершины c к стороне ab =', triag1.height('ab'))
+    print('Высота из вершины b к стороне ac =', triag1.height('ac'))
+    print('Высота из вершины a к стороне bc =', triag1.height('bc'))
+    # print('Длины сторон ab, ac, bc =', triag1._sides)
 
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
@@ -47,6 +79,13 @@ class Triangle:
 
 
 class IsoscelesTrapezoid:
-    
-    def __init__(self, p1, p2, p3, p4):
-        pass
+    """
+    Трапеция задаётся координатами четырёх её вершин -
+    a, b, c и d, представленными в видё кортежей (x, y)
+    """
+    def __init__(self, a, b, c, d):
+        if not all(map(isflatpoint, (a, b, c, d,))):
+            raise TypeError('Точка должна быть списком или кортежем координат x и y')
+        if any(map(lambda p: p[0] == p[1], zip((a, b, c, a, b, a,), (b, c, d, c, d, d,)))):
+            raise ValueError('Минимум две из заданных точек совпадают')
+        self._a, self._b, self._c, self._d = a, b, c, d
